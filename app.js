0ada023ebf577a9662ead680c9da2f17deade761
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-09-17 19:54:20 +0530

    code correction

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ app.use("/api",systemlogRoutes)
 app.use("/api",recurringExpensesRoutes)
 app.use("/api",billRoutes)
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 })
 .then(() => {
     console.log("✅ Database connected...");
