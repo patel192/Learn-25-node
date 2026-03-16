756fa1c242ac5d08497a441c64bc7926b0bb0539
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-03-16 12:01:28 +0530

    Delete Recurring endpoint

--- src/Controllers/RecurringController.js
+++ src/Controllers/RecurringController.js
@@ -1,35 +1,48 @@
 const RecurringModel = require("../models/RecurringModel");
-const createRecurring = async (req,res) => {
-    try{
-     const recurring = await RecurringModel.create(req.body);
-     res.status(201).json({
-        message:"Recurring transaction created",
-        data:recurring
-     });
-    }catch(err){
-       res.status(500).json({
-        message:"Error creating recurring transaction",
-        error:err.message
-       });
-    }
-}
+const createRecurring = async (req, res) => {
+  try {
+    const recurring = await RecurringModel.create(req.body);
+    res.status(201).json({
+      message: "Recurring transaction created",
+      data: recurring,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error creating recurring transaction",
+      error: err.message,
+    });
+  }
+};
 
+const getRecurringByUser = async (req, res) => {
+  try {
+    const recurring = await RecurringModel.find({
+      userId: req.params.userId,
+    });
+    res.status(200).json({
+      data: recurring,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error fetching recurring transactions",
+    });
+  }
+};
 
-const getRecurringByUser = async (req,res) => {
-    try{ 
-     const recurring = await RecurringModel.find({
-        userId:req.params.userId,
-     });
-     res.status(200).json({
-        data:recurring
-     })
-    }catch(err){
-     res.status(500).json({
-        message:"Error fetching recurring transactions",
-     });
-    }
-}
-
+const deleteRecurring = async (req, res) => {
+  try {
+    await RecurringModel.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Recurring transaction deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error deleting recurring transaction ,${error}`,
+    });
+  }
+};
 module.exports = {
-    createRecurring,getRecurringByUser
-}
+  createRecurring,
+  getRecurringByUser,
+  deleteRecurring
+};
--- src/routes/RecurringRoutes.js
+++ src/routes/RecurringRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Recurring = require("../Controllers/RecurringController");
 
-router.post("/recurring",Recurring.createRecurring);
-router.get("/recurring/:userId",Recurring.getRecurringByUser);
+router.post("/recurring", Recurring.createRecurring);
+router.get("/recurring/:userId", Recurring.getRecurringByUser);
+router.delete("/recurring/:id", Recurring.deleteRecurring);
 module.exports = router;
