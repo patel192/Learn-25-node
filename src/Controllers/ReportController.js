c2a7fe52ada4c9a0a1ed09c2ef5033eae1f1bd87
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-03-17 11:46:43 +0530

    Removed Admin Report

--- src/Controllers/ReportController.js
+++ src/Controllers/ReportController.js
@@ -2,40 +2,42 @@ const PDFDocument = require("pdfkit");
 const ExpenseModel = require("../models/ExpenseModel");
 const IncomeModel = require("../models/IncomeModel");
 
-const generateReport = async(req,res) => {
-  try{
-   const userId = req.params.userId;
+const generateReport = async (req, res) => {
+  try {
+    const userId = req.params.userId;
 
-   const expenses = await ExpenseModel.find({userId});
-   const income = await IncomeModel.find({userId});
+    const expenses = await ExpenseModel.find({ userId });
+    const income = await IncomeModel.find({ userId });
 
-   const totalIncome = income.reduce((a,i) => a + i.amount,0 );
-   const totalExpenses = expenses.reduce((a,e) => a + e.amount,0 )
-   
-   const doc = new PDFDocument();
+    const totalIncome = income.reduce((a, i) => a + i.amount, 0);
+    const totalExpenses = expenses.reduce((a, e) => a + e.amount, 0);
 
-   res.setHeader("Content-Type","application/pdf");
-   res.setHeader("Content-Disposition","attachment; filename=financial-report.pdf");
+    const doc = new PDFDocument();
 
-   doc.pipe(res);
-   doc.fontSize(22).text("Financial Report",{align:"center"});
-   doc.moveDown();
-   doc.fondSize(14).text(`Total Income: ${totalIncome}`);
-   doc.text(`Total Expenses: ${totalExpenses}`);
-   doc.text(`Balance: ${totalIncome - totalExpenses}`);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=financial-report.pdf",
+    );
 
-   doc.moveDown();
-   doc.text("Expenses:");
-   expenses.forEach((e)=>{
-     doc.text(`${e.title} - ${e.amount}`); 
-   });
-   doc.end();
-  }catch(error) {
-  res.status(500).json({
-    message:"Error generating report"
-  });
+    doc.pipe(res);
+    doc.fontSize(22).text("Financial Report", { align: "center" });
+    doc.moveDown();
+    doc.fondSize(14).text(`Total Income: ${totalIncome}`);
+    doc.text(`Total Expenses: ${totalExpenses}`);
+    doc.text(`Balance: ${totalIncome - totalExpenses}`);
+
+    doc.moveDown();
+    doc.text("Expenses:");
+    expenses.forEach((e) => {
+      doc.text(`${e.title} - ${e.amount}`);
+    });
+    doc.end();
+  } catch (error) {
+    res.status(500).json({
+      message: "Error generating report",
+    });
   }
-}
+};
 
-
-module.exports = {generateReport}
+module.exports = { generateReport };
--- src/routes/AdminReportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getAdminReport } = require("../Controllers/ReportController");
-const router = express.Router();
-
-router.get("/adminreport", getAdminReport);
-
-module.exports = router;
